dc7cbe4640f611ccf6909c01a4668847a0586497
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2021-01-30 19:25:47 +0000

    ft1 get data

--- Back-End/routes/ft1.js
+++ Back-End/routes/ft1.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient; 
-
+const uri = "mongodb+srv://andrewjm:andrewko08@cluster0.glqtn.mongodb.net/test";
 
 router.get('/', function (req, res, next) {
- const uri = "mongodb+srv://andrewjm:andrewko08@cluster0.glqtn.mongodb.net/Cluster0?retryWrites=true&w=majority";
- const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
- client.connect(err => {
- const collection = client.db("Goal14_DB").collection("target14_1"); 
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        client.connect(getft1Data);
 
+        function getft1Data(err){
+            if(err) console.log("Conessione al db non riuscita");
+            else{
+                const collection = client.db("Goal14_DB").collection("target14_1");
+                collection.find().toArray(callBackQuery);
+            }
 
- collection.find().limit(10).toArray((err, result) => {
-    if (err) console.log(err.message); 
-    else console.log(result);
-        client.close(); 
-    }); 
- });
- res.send('respond with a resource');
-});
+        }
+        function callBackQuery(err, result){
+            if(err) console.log(err,messagge);
+            else res.send(result);
+            client.close();
+        }
+    });
 module.exports = router;
